f3b2d3dca5cbe4cead538c8d55cedea2bf9a2af4
Mohd Ehtisham Ansari <mohammadehtisham034@gmail.com>
2025-05-21 12:34:23 +0530

    Time: 0 ms (100%), Space: 8.4 MB (38.83%) - LeetHub

---
+++ 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool areAlmostEqual(string s1, string s2) {
+        int n = s1.length();
+        int diffs = 0;
+
+        if(s1 == s2) return true;
+
+        int freq1[26] = {0};
+        int freq2[26] = {0};
+
+        for(int i=0; i<n; i++){
+            if(s1[i] != s2[i]){
+                diffs++;
+            }
+            if(diffs > 2)
+                return false;
+
+            freq1[s1[i] - 'a']++;
+            freq2[s2[i] - 'a']++;
+        }
+
+        for(int i=0; i<26; i++){
+            if(freq1[i] != freq2[i])
+                return false;
+        }
+        return true;
+
+        // TC - O(n), SC-O(26) ~ O(1)
+    }
+};
