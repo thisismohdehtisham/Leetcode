dd511f07edb68b3201aac6e734657af636d8b32e
Mohd Ehtisham Ansari <mohammadehtisham034@gmail.com>
2025-09-08 12:20:55 +0530

    Time: 0 ms (100%), Space: 13.7 MB (46.55%) - LeetHub

--- 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.cpp
+++ 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.cpp
@@ -11,22 +11,25 @@
 class Solution {
 public:
     vector<ListNode*> splitListToParts(ListNode* head, int k) {
+        // create a vector to store k parts
         vector<ListNode*> parts(k, NULL);
+
         int len = 0;
         for(ListNode* node = head; node; node = node->next) len++;
 
+        // calculate the min part size(n) and the extra nodes(r)
         int n = len / k, r = len % k;
 
         ListNode* node = head, *prev = NULL;
-
+        // loop through each part 
         for(int i=0; node && i < k; i++, r--){
-            parts[i] = node;
-            
+            parts[i] = node; //store the current node as the start of the current part
+            // traverse n+carry nodes if extra else only n nodes
             for(int j = 0; j<n + (r > 0); j++){
                 prev = node;
                 node = node -> next;
             }
-            prev-> next = NULL;
+            prev-> next = NULL; // disconnect the current part from the rest of the list 
         }
         return parts;
         
