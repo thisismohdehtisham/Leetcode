50261be93cd5c09172739e62c86fd401c8310d62
Mohd Ehtisham Ansari <mohammadehtisham034@gmail.com>
2025-09-30 12:35:30 +0530

    Time: 182 ms (93.1%), Space: 99.4 MB (81.08%) - LeetHub

---
+++ 1765-merge-in-between-linked-lists/1765-merge-in-between-linked-lists.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeInBetween(ListNode* list1, int a, int b, ListNode* list2) {
+        ListNode* firstptr = list1;
+        for(int i=0; i<a-1; i++){
+            firstptr = firstptr->next;
+        }
+
+        ListNode* secondptr = firstptr->next;
+        for(int i=0; i<b-a+1; i++){
+            secondptr = secondptr->next;
+        }
+        firstptr->next = list2;
+        while(list2->next){
+            list2 = list2->next;
+        }
+        list2->next= secondptr;
+
+        return list1;
+    }
+};
