4e33d13a8f9044064cfef1eb1da796c4260c3d3a
Mohd Ehtisham Ansari <mohammadehtisham034@gmail.com>
2025-10-06 21:19:53 +0530

    Time: 64 ms (18.54%), Space: 295.4 MB (20.68%) - LeetHub

--- 2299-merge-nodes-in-between-zeros/2299-merge-nodes-in-between-zeros.cpp
+++ 2299-merge-nodes-in-between-zeros/2299-merge-nodes-in-between-zeros.cpp
@@ -28,24 +28,5 @@ public:
 
         }
         return dummy->next;
-
-        // ListNode* dummy = new ListNode(0);
-        // ListNode* tail = dummy;
-        // ListNode* curr = head->next; // skip the first 0
-        // int sum = 0;
-
-        // while (curr) {
-        //     if (curr->val == 0) {
-        //         // segment ended, create node
-        //         tail->next = new ListNode(sum);
-        //         tail = tail->next;
-        //         sum = 0;
-        //     } else {
-        //         sum += curr->val;
-        //     }
-        //     curr = curr->next;
-        // }
-
-        // return dummy->next;
     }
 };
